2f8d704e3aacdf4840f9ce9da869f336b24e2f90
Dmitrii Osipov <dimaosipov907@gmail.com>
2023-09-14 23:06:56 +0300

    Добавьте файлы проекта.

---
+++ SFMLConsol.cpp
@@ -0,0 +1,177 @@
+﻿#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <cmath>
+#include <string>
+
+using namespace sf;
+
+bool frameCollisionX(double x, int radius) {
+    return (x + radius > 1920 || x - radius < 0);
+}
+
+bool frameCollisionY(double y, int radius) {
+    return (y + radius > 1080 || y - radius < 0);
+}
+
+double distance(Vector2f vec1, Vector2f vec2) {
+    double dx = vec1.x - vec2.x;
+    double dy = vec1.y - vec2.y;
+    return std::sqrt(dx * dx + dy * dy);
+}
+void spawnPlanet(double radius, double mass, Vector2f velocity, Vector2f position, std::vector<CircleShape>& circleVec,
+    std::vector<Vector2f>& velocities, std::vector<Vector2f>& positions, std::vector<double>& masses,
+    std::vector<int>& radiuses, std::vector<std::vector<Vector2f>> &circleTrack, Color color)
+    {
+    
+    CircleShape circle(radius);
+    circle.setFillColor(color);
+    std::vector<Vector2f> circTrack;
+    circleVec.push_back(circle);
+    velocities.push_back(velocity);
+    positions.push_back(position);
+    radiuses.push_back(radius);
+    masses.push_back(mass);
+    circleTrack.push_back(circTrack);
+}
+
+int main() {
+    std::setlocale(LC_ALL, "rus");
+    bool pause = false;
+    RenderWindow window(VideoMode(1920, 1080), "Gravity");
+    Clock clock;
+    Clock clock2;
+    Clock clock3;
+    std::vector<CircleShape> circleVec;
+    std::vector<Vector2f> velocities;
+    std::vector<Vector2f> positions;
+    std::vector<double> masses;
+    std::vector<int> radiuses;
+    std::vector<std::vector<Vector2f>> circleTrack;
+    double G = 6.67;
+    double scale = (double)2e9;
+    double low = 100;
+    double rSun = 7;
+    double F = 0;
+    double a = 0;
+    double LpsFps = 60.0f;
+    sf::Vertex track;
+    track.color = Color::Red;
+    CircleShape sun(rSun);
+    Vector2f offset(rSun, rSun);
+    double mSun = 2e30;
+    sun.setFillColor(Color::Yellow);
+    sun.setPosition(window.getSize().x / 2, window.getSize().y / 2);
+
+
+    Time timePerFrame = seconds(1.0f / LpsFps); // lps
+    Time timePerFrame2 = seconds(1.0f / LpsFps); // fps
+    Time accumulatedTime = Time::Zero;
+    Time accumulatedTime2 = Time::Zero;
+
+    while (window.isOpen()) {
+        Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed) {
+                window.close();
+            }
+            
+            if (event.type == Event::KeyReleased && event.key.code == Keyboard::Num1) {
+                
+                spawnPlanet(5, 3.33e23, Vector2f(0, 7.8), Vector2f(sun.getPosition().x + 29, sun.getPosition().y), std::ref(circleVec), std::ref(velocities),
+                    std::ref(positions), std::ref(masses), std::ref(radiuses), std::ref(circleTrack), Color::Color(128,128,128));
+                
+                spawnPlanet(5, 4.87e24, Vector2f(0,5.8), Vector2f(sun.getPosition().x + 54, sun.getPosition().y), std::ref(circleVec), std::ref(velocities),
+                    std::ref(positions), std::ref(masses), std::ref(radiuses), std::ref(circleTrack), Color::Color(234, 205, 177));
+                
+                spawnPlanet(5, 5.97e24, Vector2f(0, 5), Vector2f(sun.getPosition().x + 75, sun.getPosition().y), std::ref(circleVec), std::ref(velocities),
+                    std::ref(positions), std::ref(masses), std::ref(radiuses), std::ref(circleTrack), Color::Color(154, 205, 50));
+                
+                spawnPlanet(5, 6.42e23, Vector2f(0, 4), Vector2f(sun.getPosition().x + 114, sun.getPosition().y), std::ref(circleVec), std::ref(velocities),
+                    std::ref(positions), std::ref(masses), std::ref(radiuses), std::ref(circleTrack), Color::Color(228, 64, 3));
+                
+                spawnPlanet(5, 1.89e27, Vector2f(0, 1.8), Vector2f(sun.getPosition().x + 389, sun.getPosition().y), std::ref(circleVec), std::ref(velocities),
+                    std::ref(positions), std::ref(masses), std::ref(radiuses), std::ref(circleTrack), Color::Color(255, 226, 183));
+                
+                
+                spawnPlanet(5, 5.68e26, Vector2f(0, 1.4), Vector2f(sun.getPosition().x + 700, sun.getPosition().y), std::ref(circleVec), std::ref(velocities),
+                    std::ref(positions), std::ref(masses), std::ref(radiuses), std::ref(circleTrack), Color::Color(255, 219, 139));
+                /*
+                spawnPlanet(5, 8.68e25, Vector2f(0, 1), Vector2f(sun.getPosition().x + 1400, sun.getPosition().y), std::ref(circleVec), std::ref(velocities),
+                    std::ref(positions), std::ref(masses), std::ref(radiuses), std::ref(circleTrack), Color::Color(150,229,233));
+                   */ 
+                 
+            } else if (event.type == Event::KeyReleased && event.key.code == Keyboard::Num2)
+            {
+                for (int i = 0; i < circleVec.size(); i++) {
+                    circleTrack[i].clear();
+                }
+            } else if (event.type == Event::KeyReleased && event.key.code == Keyboard::Num3)
+            {
+                for (int i = 0; i < circleVec.size(); i++) {
+                    circleVec.clear();
+                    circleTrack.clear();
+                    masses.clear();
+                    positions.clear();
+                    velocities.clear();
+                    radiuses.clear();
+                }
+            } else if (event.type == Event::KeyReleased && event.key.code == Keyboard::Space) {
+                
+                pause = !pause;
+            }
+        }
+        
+        accumulatedTime += clock.restart();
+        while (accumulatedTime >= timePerFrame) {
+            accumulatedTime -= timePerFrame;
+            
+            if (!pause) {
+                for (int i = 0; i < circleVec.size(); i++) {
+                    //Vector2f pos(positions[i].x + radiuses[i], positions[i].y + radiuses[i]);
+                    //circleTrack[i].push_back(pos);
+                    if (circleTrack[i].size() > 1) {
+                        //circleTrack[i].erase(circleTrack[i].begin());
+                    }
+                    double distan = distance(sun.getPosition() + offset, positions[i] + Vector2f(radiuses[i], radiuses[i])) * scale;
+                    Vector2f normalizeDirection;
+                    F = G * (mSun * masses[i]) / pow(distan, 2);
+                    a = F / masses[i];
+                    normalizeDirection = ((positions[i] + Vector2f(radiuses[i], radiuses[i])) - (sun.getPosition() + offset)) / (float)distan;
+                    velocities[i] -= normalizeDirection * static_cast<float>(a);
+                    positions[i] += velocities[i];
+
+                    if (distan <= radiuses[i] + rSun) {
+                        circleVec.erase(circleVec.begin() + i);
+                        positions.erase(positions.begin() + i);
+                        radiuses.erase(radiuses.begin() + i);
+                        masses.erase(masses.begin() + i);
+                        velocities.erase(velocities.begin() + i);
+                        circleTrack.erase(circleTrack.begin() + i);
+                    }
+                }
+            }
+        }
+        std::cout << "///" << circleVec.size() << "\n";
+        // Отрисовка с учетом интерполяции
+        accumulatedTime2 += clock2.restart();
+        if (accumulatedTime2 >= timePerFrame2) {
+            window.clear(Color::Black);
+            for (int i = 0; i < circleVec.size(); i++) {
+                for (int j = 0; j < circleTrack[i].size(); j++) {
+                    track.color = circleVec[i].getFillColor();
+                    track.position = circleTrack[i][j];
+                    window.draw(&track, 1, Points);
+                }
+                circleVec[i].setPosition(positions[i]);
+                window.draw(circleVec[i]);
+            }
+            window.draw(sun);
+
+            window.display();
+            accumulatedTime2 -= timePerFrame2;
+        }
+    }
+
+    return 0;
+}
